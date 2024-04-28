1e8bee951482391d0805801ead3d4b3bc5709950
mahdioudi <148377141+mahdioudi@users.noreply.github.com>
2024-04-28 20:36:44 +0330

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,11 +1,12 @@
 public class Person {
     private String firstName;
     private String lastName;
-    Person(String firstName, String lastName){
-        this.firstName=firstName;
-        this.lastName=lastName;
 
+    public Person(String firstName , String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
+
     public String getFirstName() {
         return firstName;
     }
